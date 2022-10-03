24650365c4e23936ad6b3aaae377ec30462c913f
Kacper Janiuk <kacper.janiuk@cetuspro.pl>
2022-10-03 23:46:04 +0200

    Adding folder styles, where created globalStyles

---
+++ src/components/atoms/Links/CustomLink.jsx
@@ -0,0 +1,7 @@
+const CustomLink = () => {
+    return (
+        <div>Custom Link</div>
+    )
+}
+
+export default CustomLink
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../assets/styles/GlobalStyles";
+import { theme } from "../assets/styles/theme";
+import { routes } from "../routes";
+import ViewHome from "./Home/ViewHome";
 
-const App = () =>{
+
+const App = () => {
   return (
-    <div >
-      <header >
-        <p>
-          Hello world
-        </p>
-
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Routes>
+        <Route path={routes.home} element={<ViewHome />} />
+      </Routes>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
