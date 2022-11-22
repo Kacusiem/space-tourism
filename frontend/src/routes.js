c6a400a69314db58715e86eaa16e81b9215efa12
Kacper Janiuk <kacper.janiuk@cetuspro.pl>
2022-11-22 21:56:04 +0100

    Adding redirects to the appropriate subpages and creating the basic component of the subpage, adding the function responsible for the appropriate redirection after the identifier

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,26 @@
 export const routes = {
-    home: '/',
-    register: '/register',
-    login: '/login',
-    destinations: '/user/destinations',
-    destination: '/user/destination/:id',
-    crews: '/user/crews',
-    crew: '/user/crew/:id',
-    technologies: '/user/technologies',
-    technology: '/user/technology/:id',
-}
+  home: "/",
+  register: "/register",
+  login: "/login",
+  destinations: "/user/destinations",
+  destination: "/user/destination/:id",
+  crews: "/user/crews",
+  crew: "/user/crew/:id",
+  technologies: "/user/technologies",
+  technology: "/user/technology/:id",
+};
+
+export const passParams = (route, params) => {
+  const parts = route.split("/");
+  let newRoute = "";
+  for (let i = 1; i < parts.length; i++) {
+    newRoute += "/";
+    if (parts[i].includes(":")) {
+      newRoute += params[parts[i].replace(":", "")];
+    } else {
+      newRoute += parts[i];
+    }
+  }
+
+  return newRoute;
+};
